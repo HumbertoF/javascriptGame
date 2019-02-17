cfd515f58707149cf6d16d5a22b2bcbd891f3854
HumbertoF <hflores@nhaschools.com>
2019-02-17 12:53:29 -0500

    labPractice, first draft

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// Ask user if wants to play the game or not
+{ 
+ let greeting = prompt("Would you like to play a game?");
+
+// Declare variables and ask for user name
+
+if (greeting.toLowerCase() === "yes") {
+    let userHealth = 40;
+    let grantHealth = 10;
+    let userName = prompt("What is your name?");
+    let wins = 0;
+
+// Print a welcome message to start the game
+    console.log(`Let's Begin! ${userName}'s Starting Health is ${userHealth}, and Grant's Starting Health is ${grantHealth}`);
+
+// While loop 
+    while (userHealth >= 0 && wins < 3) {
+        
+        if (grantHealth <= 0) {
+            grantHealth = 10;
+            wins++;
+            console.log( "Grant has been defeated" );
+        } else 
+
+        {   
+        grantHealth -= Math.floor(Math.random()* 2 + 1);
+        userHealth-= Math.floor(Math.random() * 2 + 1);
+        console.log(`${userName}'s Health: ${userHealth}`); 
+        console.log(`Grant's Health: ${grantHealth}`);
+        }   
+    }
+    
+    // validate the counts and print out a congratulation message
+    if (wins === 3){
+        console.log(`Grants has been defetead 3 times. That means that you are the winner ${userName}.  CONGRATULATIONS!!!!`);
+    }
+
+}
+// if user does not want to play print out the following message
+ else if (greeting === "no") {
+    console.log("Maybe next time?");pageYOffset
+    }
+}
